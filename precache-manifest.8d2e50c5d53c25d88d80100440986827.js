a5f5062b8e1b203fb2d23224b1908e063953d0a8
Rohit <therohitjha@gmail.com>
2020-06-09 19:22:30 +0530

    Updates

---
+++ precache-manifest.8d2e50c5d53c25d88d80100440986827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b68cfc0229ef760f3d54d7600c329c8",
+    "url": "/post-comment-app/index.html"
+  },
+  {
+    "revision": "e3782de611586281d170",
+    "url": "/post-comment-app/static/css/main.a5fba7a3.chunk.css"
+  },
+  {
+    "revision": "062adbabdb7be0d07448",
+    "url": "/post-comment-app/static/js/2.e33a794a.chunk.js"
+  },
+  {
+    "revision": "49c45e15b22906ea61e3b3085d227f58",
+    "url": "/post-comment-app/static/js/2.e33a794a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3782de611586281d170",
+    "url": "/post-comment-app/static/js/main.01719972.chunk.js"
+  },
+  {
+    "revision": "094cf34aacae2a476686",
+    "url": "/post-comment-app/static/js/runtime-main.32da131e.js"
+  }
+]);
